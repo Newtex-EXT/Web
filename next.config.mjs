9aa55e4f501412263a7fe7f73fe0d528b103cabb
NewTex <228597739+NewTexEXT@users.noreply.github.com>
2026-02-02 13:28:02 +0100

    Añadir serverExternalPackages a nextConfig para que acepte librerias externas

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Se ha movido aquí en las versiones más recientes
+  serverExternalPackages: ['pino', 'pino-pretty', 'thread-stream'],
+};
 
 export default nextConfig;
